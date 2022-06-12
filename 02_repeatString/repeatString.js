67f4dde0ff499daf0d5a59140ccb96dee81bd56c
aliagha <aghajani.dev@gmail.com>
2022-06-12 15:05:27 +0430

    test 4 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
 
+const repeatString = function(str,num) {
+    if(num === 0 || str===''){
+       return str=''
+    }else if(num === -1){
+         return "ERROR";
+    }else {
+        return str.repeat(num);
+    }
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...Arrgs) {
+    
+    Arrgs.forEach(arg => {  
+      arr.forEach (x => {
+    
+        if (x === arg) {
+          arr.splice(arr.indexOf(x),1);
+        };
+      });
+    });
+ return arr;
 };
 
 // Do not edit below this line
