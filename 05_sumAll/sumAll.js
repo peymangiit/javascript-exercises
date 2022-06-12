05a03208861c2ec7d9cc25a7620ec5d5af420147
aliagha <aghajani.dev@gmail.com>
2022-06-12 23:20:32 +0430

    add code to test5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1 , num2) {
+    if(num1<0 || num2<0 || typeof(num1)!=="number" || typeof(num2)!=="number"){
+        return "ERROR";
 
+      }else if (num1<num2){
+        for (let i=num1+1 ; i<=num2;i++)
+          num1 += i;
+
+        }else {
+          for (i=num1-1;i>=num2;i--)
+          num1 +=i;
+        
+      }
+      return num1;
 };
 
 // Do not edit below this line
