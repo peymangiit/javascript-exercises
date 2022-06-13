251bad6e254b5a1b61fa0a35aef458dc9cd73e38
aliagha <aghajani.dev@gmail.com>
2022-06-13 22:58:24 +0430

    add code for test 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0 ){
+        return "OOPS"
+    }else if (typeof(n)=="string"){
+        n= Number(n);
+    }
+    const result = [0, 1];
+    for (let i = 2; i <= n; i++) {
+      result.push(result[i-2] + result[i-1]);
+    }
+    return result[n];
 };
 
 // Do not edit below this line
