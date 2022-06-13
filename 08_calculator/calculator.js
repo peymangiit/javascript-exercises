0130a193d2517ef5acd2770a8dd12e91792e824e
aliagha <aghajani.dev@gmail.com>
2022-06-13 14:48:53 +0430

    add code for test8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return (num1)-(num2);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	array.forEach(num => 
+     sum += num );
+    return sum;
+
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let multiply = 1;
+  array.forEach(num => 
+    multiply *= num);
+    return multiply;
 
 };
 
-const power = function() {
-	
+const power = function(num1 ,num2) {
+	return Math.pow(num1 , num2) ;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 ){
+    return 1;
+  } else {
+    return num*factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
