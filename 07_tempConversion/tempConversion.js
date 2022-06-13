a7fb20b18871b983ba1ccd293d443b7d0775723a
aliagha <aghajani.dev@gmail.com>
2022-06-13 14:29:19 +0430

    add code for test7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+ let  celsius = (fah - 32) * (5/9);
+  celsius = Math.round(celsius *10) / 10;
+ return celsius; 
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let fahrenhiet = (cel * (9/5)) + 32;
+  fahrenhiet = Math.round(fahrenhiet *10) / 10;
+  return fahrenhiet;
 };
 
 // Do not edit below this line
