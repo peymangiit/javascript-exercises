0138f07edea9ab2e3f2579ac5d497b16ba09091c
aliagha <aghajani.dev@gmail.com>
2022-06-14 15:01:53 +0430

    add code for test 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+ const getAge = ({yearOfBirth , yearOfDeath}) => {
 
+    //condition if the person still alive
+    if (yearOfDeath === ''){ 
+        yearOfDeath=new Date().getFullYear()
+    }
+    return yearOfDeath-yearOfBirth;
+ }
+ let oldestAge = getAge(people[0])
+
+ //going through every object in array and compare  
+ let oldestPerson = people.reduce((oldest,current) => {
+   let currentAge = getAge(current)
+    if (currentAge > oldestAge) {
+        oldestAge = currentAge;
+        return current;
+    }
+    return oldest;
+ });
+ return oldestPerson;
 };
 
 // Do not edit below this line
